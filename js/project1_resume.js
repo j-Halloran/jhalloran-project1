af2a9d338fc60161276f2216e2ba1e27af4ceb0d
j-halloran <jakehalloran2@gmail.com>
2016-10-13 19:54:56 -0400

    Added resume and jscript

---
+++ js/project1_resume.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  setTitleDate(); //Fucntion to set the last updated date
+});
+
+function setTitleDate(){
+  var dayInSeconds = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+  var loadTime = new Date();
+  var lastUpdate = new Date(2016,10,02);
+  var diffDays = Math.round(Math.abs((loadTime.getTime() - lastUpdate.getTime())/(dayInSeconds)));
+  alert(loadTime.getDay());
+  $("#resumeHead").text("Resume - Last Updated "+diffDays+" Days Ago");
+  return true;
+}
