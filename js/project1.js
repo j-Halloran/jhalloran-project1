ff5c61840b1e3394b09d9e3927e3428d17ada437
j-halloran <jakehalloran2@gmail.com>
2016-10-13 01:14:33 -0400

    Further updated contact form and js

--- js/project1.js
+++ js/project1.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
   //Hide the error and success messages to start
   $("#errorHolder").hide();
   $("#successHolder").hide();
+  $("#formTitle").show();
 
   //Event listener for submit button
   $("#commentForm").submit(function(event){
     if(verifyForm()){
       $("#formHolder").hide();
       $("#errorHolder").hide();
+      $("#formTitle").hide();
       $("#successHolder").show();
     }
 
@@ -20,6 +22,9 @@ $(document).ready(function(){
 });
 
 function verifyForm(){
+  //Hide the titlePara
+  $("#formTitle").hide();
+
   //Hide each individual error
   $("#nameError").hide();
   $("#emailError").hide();
@@ -28,24 +33,28 @@ function verifyForm(){
 
   //check for name length
   if($("input[name='name-field']").val().length==0){
+    $("formTitle").hide();
     $("#nameError").show();
     return false;
   }
 
   //check for email length
   else if($("input[name='email-field']").val().length==0){
+    $("formTitle").hide();
     $("#emailError").show();
     return false;
   }
 
   //check for @ in email
   else if($("input[name='email-field']").val().indexOf('@')==-1){
+    $("formTitle").hide();
     $("#emailFormatError").show();
     return false;
   }
 
   //Check for comment length
-  else if($("input[name='comment-field']").length==0){
+  else if($("textarea[name='comment-field']").val().length==0){
+    $("formTitle").hide();
     $("#commentError").show();
     return false;
   }
